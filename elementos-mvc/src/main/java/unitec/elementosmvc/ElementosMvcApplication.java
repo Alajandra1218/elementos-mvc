da4c33231fe7a203a1e75c610405f34bd6b2aec8
Alejandra1218 <35473667+Alajandra1218@users.noreply.github.com>
2018-05-29 21:44:52 -0500

    Add files via upload

---
+++ elementos-mvc/src/main/java/unitec/elementosmvc/ElementosMvcApplication.java
@@ -0,0 +1,48 @@
+package unitec.elementosmvc;
+
+import java.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementosMvcApplication implements CommandLineRunner{
+
+    @Autowired ServicioTarjeta servicio;
+    @Autowired RepositorioMensaje repoMensaje;
+    
+	public static void main(String[] args) {
+		SpringApplication.run(ElementosMvcApplication.class, args);
+                
+                
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+     
+         //servicio.obtenerSaldo().obtenerSaldo();
+         //LocalDate fecha=LocalDate.now();
+       // repoMensaje.save(new Mensaje("hola",fecha,"cerdito"));
+       //2.buscar todos los mensajes ya
+      
+             //for(Mensaje mensa: repoMensaje.findAll())
+            // {
+                // System.out.println(mensa);
+           //  }
+
+       //3.Mensaje por ID
+
+        //System.out.println(repoMensaje.findById("5b0dfe1fc4d6f30dec4e1976").get());
+       //3.1 buscar por otro campo
+         //  System.out.println(repoMensaje.findByCuerpo("cerdito"));
+       //4.Actualizar un mensaje
+       
+       //5.Eliminar un mensaje
+       
+       // Mensaje e=new Mensaje();
+       //e.setId("hola");
+       
+      // repoMensaje.deleteById("5b0e0055c4d6f30dec4e1978");
+    }
+}
---
+++ elementos-mvc/src/main/java/unitec/elementosmvc/Mensaje.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec.elementosmvc;
+
+import java.time.LocalDate;
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author T-107
+ */
+public class Mensaje {
+    
+    @Id
+    private String id;
+    
+    private LocalDate fecha;
+    private String cuerpo;
+
+    public Mensaje(LocalDate fecha, String cuerpo) {
+        this.fecha = fecha;
+        this.cuerpo = cuerpo;
+    }
+
+    public Mensaje(String id, LocalDate fecha, String cuerpo) {
+        this.id = id;
+        this.fecha = fecha;
+        this.cuerpo = cuerpo;
+    }
+
+  //constructor de defecto  
+    public Mensaje() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensaje{" + "id=" + id + ", fecha=" + fecha + ", cuerpo=" + cuerpo + '}';
+    }
+    
+    
+}
